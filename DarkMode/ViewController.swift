6288462b1514a6ee0f097a77e3f1865b81acd712
Enes Kaya <eng.eneskaya@gmail.com>
2022-07-17 23:39:48 +0300

    Dark Mode

--- DarkMode/ViewController.swift
+++ DarkMode/ViewController.swift
@@ -9,9 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let userInterfaceStyle = traitCollection.userInterfaceStyle
+        
+        if userInterfaceStyle == .dark {
+            button.tintColor = .white
+            
+            
+        }
+        else{
+            button.tintColor = .black
+        }
+    }
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        let userInterfaceStyle = traitCollection.userInterfaceStyle
+        
+        if userInterfaceStyle == .dark {
+            button.tintColor = .white
+            
+            
+        }
+        else{
+            button.tintColor = .black
+        }
     }
 
 
